6716aefa6373212b4135d1dd3e9652d8f6cf99d3
Vyacheslav Bezborodov <vyacheslav.bezborodov@gmail.com>
2017-05-20 12:07:47 +0500

    add parallel plot

--- utils/plot.py
+++ utils/plot.py
@@ -9,7 +9,6 @@ def plot():
     for point in zip(cores2, cores4, cores8):
         plt.plot([2, 4, 8], point)
     plt.xticks([2, 4, 8])
-    plt.title("График параллельных координат")
     plt.xlabel('Количество потоков')
     plt.ylabel('Ускорение')
     plt.savefig(os.path.join("plots", "parallel.png"), dpi=480)
