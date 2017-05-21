3f182d81b8f69ff5de0a28efe8aec9da7af8a30f
Vyacheslav Bezborodov <vyacheslav.bezborodov@gmail.com>
2017-05-21 20:41:53 +0500

    fix plot

--- utils/plot.py
+++ utils/plot.py
@@ -2,14 +2,15 @@ import os
 
 import matplotlib.pyplot as plt
 
-from data import *
+from data import cores2, cores4, cores8
+from utils import normalize
 
 
 def plot():
-    for point in zip(cores2, cores4, cores8):
-        plt.plot([2, 4, 8], point)
-    plt.xticks([2, 4, 8])
-    plt.xlabel('Количество потоков')
-    plt.ylabel('Ускорение')
+    ncores = [2, 4, 8]
+    for point in zip(normalize(cores2), normalize(cores4), normalize(cores8)):
+        plt.plot(ncores, point)
+    plt.xticks(ncores)
+    plt.xlabel("Количество потоков")
+    plt.ylabel("Ускорение (нормализация по СКО)")
     plt.savefig(os.path.join("plots", "parallel.png"), dpi=480)
-    plt.show()
--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,21 @@
-import functools
-import random
+from functools import reduce
+from random import uniform
 
-def geomean(nums):
-    return round((functools.reduce(lambda x, y: x*y, nums))**(1.0/len(nums)), 2)
+import numpy as np
 
-def mean(vals):
-    return round(sum(vals)/len(vals), 2)
 
-def trans(vals):
-    return [round(v + random.uniform(-0.07, 0.0), 2) for v in vals]
+def geomean(vs):
+    return round((reduce(lambda x, y: x * y, vs)) ** (1.0 / len(vs)), 2)
+
+
+def mean(vs):
+    return round(sum(vs) / len(vs), 2)
+
+
+def normalize(vs):
+    scale = np.mean(vs)
+    return [v / scale for v in vs]
+
+
+def trans(vs):
+    return [round(v + uniform(-0.07, 0.0), 2) for v in vs]
